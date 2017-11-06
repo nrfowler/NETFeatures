6f7acc86235e7bb6ecaae51b0d360d21a88048eb
nrfowler <nrfowler@gmail.com>
2017-11-05 20:12:00 -0600

    Add project files.

---
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+    using System.Web.Http;
+
+namespace WebApplication1.App_Start
+{
+
+    class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling
+= Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+    }
+}
